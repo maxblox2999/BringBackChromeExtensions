c8d6d9eb5167218d8e193d1d7b6de4ad725132d5
Max <maxbonello.apple@gmail.com>
2026-09-23 14:54:46 +0000

    fixed sum stuff

--- EnableMV2.ps1
+++ EnableMV2.ps1
@@ -1,9 +1,36 @@
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Warning "Run this script as Administrator."
-    break
+#Requires -Version 5.1
+$ErrorActionPreference = 'Stop'
+
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = [Security.Principal.WindowsPrincipal]::new($identity)
+$isAdministrator = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdministrator) {
+    throw 'Run this script as Administrator.'
+}
+$chromePaths = @(
+    "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"
+    "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+    "$env:LOCALAPPDATA\Google\Chrome\Application\chrome.exe"
+)
+
+$chromePath = $chromePaths | Where-Object { Test-Path -LiteralPath $_ } | Select-Object -First 1
+if (-not $chromePath) {
+    throw 'Google Chrome was not found in a standard install location.'
 }
 
-New-Item -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name "ExtensionManifestV2Availability" -Value 2 -PropertyType DWord -Force
+$chromeVersion = [version](Get-Item -LiteralPath $chromePath).VersionInfo.ProductVersion
+if ($chromeVersion.Major -ge 139) {
+    throw "Chrome $chromeVersion does not support Manifest V2. Chrome removed this policy in version 139."
+}
 
-Write-Host "Manifest V2 enabled. Restart Chrome to apply changes." -ForegroundColor Green
+$policyPath = 'HKLM:\SOFTWARE\Policies\Google\Chrome'
+$policyName = 'ExtensionManifestV2Availability'
+New-Item -Path $policyPath -Force | Out-Null
+New-ItemProperty -Path $policyPath -Name $policyName -Value 2 -PropertyType DWord -Force | Out-Null
+$savedValue = Get-ItemPropertyValue -Path $policyPath -Name $policyName
+if ($savedValue -ne 2) {
+    throw "The policy write failed. Expected 2, found $savedValue."
+}
+
+Write-Host "Policy set for Chrome $chromeVersion. Restart Chrome, then check chrome://policy." -ForegroundColor Green
