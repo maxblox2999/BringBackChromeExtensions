166627341ab5a6a07b8c337e8e2106e6d592156d
Max <maxbonello.apple@gmail.com>
2026-09-23 14:56:48 +0000

    fixed some issues with it

--- EnableMV2.ps1
+++ EnableMV2.ps1
@@ -1,6 +1,6 @@
 #Requires -Version 5.1
 $ErrorActionPreference = 'Stop'
-
+ 
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = [Security.Principal.WindowsPrincipal]::new($identity)
 $isAdministrator = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
